56cfa88b1cc623806187fc3a2eb979a2e51d2cc0
WShimoda <granstonberrygetmail@gmail.com>
2016-09-19 21:05:14 +0900

    1.1.1.2

--- chat/main.go
+++ chat/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>Chat</title>
-        </head>
-        <body>
-          Let's Chat!
-        </body>
-      </html>
-    `))
+	// root
+	http.Handle("/", &templateHandler{
+		filename: "chat.html",
 	})
 	// Run Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {

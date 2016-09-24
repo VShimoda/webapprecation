052ebfdda1859a412ceb347831d890cac41037c6
WShimoda <granstonberrygetmail@gmail.com>
2016-09-24 20:28:57 +0900

    1.3.1

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -18,10 +19,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "application address")
+	flag.Parse()
 	r := newRoom()
 	// room
 	http.Handle("/", &templateHandler{
@@ -31,7 +34,8 @@ func main() {
 	// start room
 	go r.run()
 	// Run Web Server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Web server running. Port:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }

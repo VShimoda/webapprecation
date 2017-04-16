5c405d3584558e33d6ad760fd09fa2a842ab8545
WShimoda <granstonberrygetmail@gmail.com>
2017-04-16 16:03:43 +0900

    2.6

--- chat/main.go
+++ chat/main.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -25,6 +29,29 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "application address")
 	flag.Parse()
+
+	// Gomniauth
+	// random value for secret
+	gomniauth.SetSecurityKey("vshimoda")
+	// oauth2
+	gomniauth.WithProviders(
+		facebook.New(
+			"318772611789665",
+			"620c0b2ad1f21c7a1ac60540e02f8cbc",
+			"http://localhost:8080/auth/callback/facebook",
+		),
+		github.New(
+			"a64d23d1fb23faebd34a",
+			"64249bfa218997b205076c9021b7bda9aca33770",
+			"http://localhost:8080/auth/callback/github",
+		),
+		google.New(
+			"577006250786-e2ina0bireqdu9g9v4qhdaa7hqqassu9.apps.googleusercontent.com",
+			"Xj09MUsqQ4Rl2D8L3xR3_qos",
+			"http://localhost:8080/auth/callback/google",
+		),
+	)
+
 	r := newRoom()
 	// room
 	//http.Handle("/", &templateHandler{
